4a2221c3958012c78c88283cdbe6bfe0ed2d64f1
jgonz19 <90273230+jgonz19@users.noreply.github.com>
2021-11-18 17:01:31 +0100

    updating database

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-objective-80043',
-      //port : 3000,
-      user : 'postgres',
-      password : 'admin',
-      database : 'smart-brain'
+      connectionString : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
